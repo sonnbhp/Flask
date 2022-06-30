b7d9b3f211cb6f67c61ca6a73bd5e7a621fd7cf9
sonnbhp <sonnbhp@gmail.com>
2022-06-30 23:04:30 +0700

    ngay 2

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-# save this as app.py
-from flask import Flask
+
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+
+@app.route("/admin")
+def hello_admin():
+    return f"<h1> Hello admin </h1>"
+@app.route("/user/<name>") #chuyền biến name
+def hello_user(name):
+    if name == "admin":
+        #chuyển trang
+        return redirect(url_for("hello_admin"))
+    return f"<h1> Hello {name} </h1>"
+
+@app.route("/blog/<int:id>") #Truyền ID
+def blog(id):
+    return f"<h1> Hello {id} </h1>"
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    #return render_template("index.html", content = "flask", cars =["Vìn", "Honda", "Mec"])
+    return render_template("home.html")
+@app.route("/login", methods = ["POST","GET"])
+def login():
+    if request.method == "POST":
+        user_name = request.form["name"]
+        if user_name:
+            return redirect(url_for("hello_user", name=user_name))
+    return render_template("login.html")
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
